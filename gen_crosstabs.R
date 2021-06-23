787a3b2a594b995df1467cf2a9d68d9324403188
Sam Crawley <sam@crawley.nz>
2021-06-23 16:53:20 +1200

    Add counts/missing for the summary page

--- gen_crosstabs.R
+++ gen_crosstabs.R
@@ -4,7 +4,12 @@ library(openxlsx)
 library(countrycode)
 library(haven)
 
-skip.countries <- c("CHN", "EGY", "VNM")
+skip.countries <- c("CHN", "EGY", "VNM", "JOR")
+
+questions <- c("Q272", "Q289", "Q290")
+q.names <- c("Language", "Religion", "Ethnicity")
+names(q.names) <- questions 
+names(questions) <- q.names
 
 gen.wvs.crosstabs <- function(write.res = T, lump = T) {
   data <- read_dta("Divided/data/orig/WVS_Cross-National_Wave_7_stata_v1_6_2.dta", encoding = "UTF-8") %>%
@@ -44,13 +49,23 @@ gen.wvs.crosstabs <- function(write.res = T, lump = T) {
     
     names(tables) <- c("Language", "Relgion", "Ethnicity")
     
-    tables[['Sample Size']] <- nrow(d)
+    cors <- calc.correlations(d)
+    group.var <- questions[[cors$max.col]]
+    missing.counts <- d %>% summarise(
+      party.missing.n = sum(Q223 == '(Missing)'),
+      party.missing.pct = sum(Q223 == '(Missing)') / length(Q223),
+      group.missing.n = sum(.data[[group.var]] == '(Missing)'),
+      group.missing.pct = sum(.data[[group.var]] == '(Missing)') / length(.data[[group.var]]),
+      group.party.n = sum(Q223 != '(Missing)' & .data[[group.var]] != '(Missing)'),
+      group.party.pct = sum(Q223 != '(Missing)' & .data[[group.var]] != '(Missing)') / length(Q223),
+    )
     
     tables$Summary <- list(
       'Country' = countrycode(country, origin = 'iso3c', destination = 'country.name'),
       'Year' = unique(as.numeric(d$A_YEAR)),
       'Sample Size' = nrow(d),
-      'Correlations' = calc.correlations(d)
+      'Correlations' = cors,
+      'Missing Counts' = missing.counts
     )
     
     tables
@@ -69,17 +84,16 @@ gen.wvs.crosstabs <- function(write.res = T, lump = T) {
   return(res)
 }
 
-calc.correlations <- function(d) {
-  questions <- c("Q272", "Q289", "Q290")
-  q.names <- c("Language", "Religion", "Ethnicity")
-  names(q.names) <- questions  
-  
-  tau <- map_dfr(questions, function(var) {
+calc.correlations <- function(d, forward = T) {
+  tau <- map_dfr(unname(questions), function(var) {
     t <- GKtau(d[[var]], d$Q223)
-    tibble(question = q.names[[var]], forward = t$tauxy, back = t$tauyx)
-  }) %>%
-    pivot_longer(c(forward, back)) %>% 
-    pivot_wider(names_from = c(question, name)) %>%
+    tibble(question = q.names[[var]], assoc = ifelse(forward, t$tauxy, t$tauyx))
+  })
+
+  tau <- tau %>%
+    pivot_longer(assoc) %>% 
+    pivot_wider(names_from = question) %>%
+    select(-name) %>%
     mutate(across(everything(), ~ifelse(is.nan(.x) | .x == -Inf, NA, .x))) %>%
     mutate(max.col = names(.)[which.max(c_across(everything()))])
   
@@ -95,7 +109,8 @@ write.wvs.xlsx <- function(res) {
   hs2 <- createStyle(textDecoration = "bold")
   
   addWorksheet(wb, "Summary")
-  summary.headers <- c("Country", "Survey Year", "Sample Size", "Group Basis", "Group Assoc.")
+  summary.headers <- c("Country", "Survey Year", "Sample Size", "Group Basis", "Group Assoc.", "Incl. Group (N)", "Incl. Group (%)",
+                       "Party Missing (N)", "Party Missing (%)", "Group Missing (N)", "Group Missing (%)")
   writeData(wb, "Summary", data.frame(t(summary.headers)), startRow = 1, startCol = 1, colNames = F, rowNames = F)
   country.count <- 1
   
@@ -149,7 +164,7 @@ write.wvs.xlsx <- function(res) {
     writeData(wb, country, "Sample Size", startCol = 1, startRow = startRow)
     addStyle(wb, sheet = country, hs1, rows = startRow, cols = 1)
     
-    writeData(wb, country, res[[country]][['Sample Size']], startCol = 1, startRow = startRow+1)
+    writeData(wb, country, res[[country]]$Summary$`Sample Size`, startCol = 1, startRow = startRow+1)
   }
   
   saveWorkbook(wb, "Divided/data/output/wvs_crosstabs.xlsx", overwrite = T)
@@ -170,6 +185,13 @@ get.country.summary.line <- function(country.data) {
   }
   
   sum.line$`Group Assoc` <- summary.data$Correlations %>% pull({{max.col}})
+  
+  sum.line$`PartGrp` <- summary.data$`Missing Counts`$group.party.n
+  sum.line$`PartGrp %` <- round(summary.data$`Missing Counts`$group.party.pct, 2) * 100
+  sum.line$`Missing Party` <- summary.data$`Missing Counts`$party.missing.n
+  sum.line$`Missing Party %` <- round(summary.data$`Missing Counts`$party.missing.pct, 2) * 100
+  sum.line$`Missing Group` <- summary.data$`Missing Counts`$group.missing.n
+  sum.line$`Missing Group %` <- round(summary.data$`Missing Counts`$group.missing.pct, 2) * 100
   
   unlist(sum.line)
 }
