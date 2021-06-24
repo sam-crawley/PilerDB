f7ada61689fe294fbb730e488d07d54d71d6de0f
Sam Crawley <crawlesa@staff.vuw.ac.nz>
2021-06-25 10:43:32 +1200

    Allow correlations to be calculated as forward, back or both

--- correlations.R
+++ correlations.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 library(GoodmanKruskal)
+library(countrycode)
 
 skip.countries <- c("CHN", "EGY", "VNM")
 
-gen.correlations <- function() {
+gen.correlations <- function(direction = "back") {
   data <- readRDS("Divided/data/orig/WVS_Cross-National_Wave_7_R_v1_6.rds") %>%
     filter(! B_COUNTRY_ALPHA %in% skip.countries)
   
@@ -18,24 +19,38 @@ gen.correlations <- function() {
     tau <- map_dfr(questions, function(var) {
       t <- GKtau(d[[var]], d$Q223)
       tibble(question = q.names[[var]], forward = t$tauxy, back = t$tauyx)
-    }) %>%
+    }) 
+    
+    if (! is.na(direction)) {
+      tau <- tau %>% select(question, {{direction}}) %>%
+        pivot_longer({{direction}}) %>%
+        pivot_wider(names_from = c(question)) %>%
+        select(-name)
+    }
+    else {
+      tau <- tau %>%
       pivot_longer(c(forward, back)) %>% 
       pivot_wider(names_from = c(question, name))
+    }
     
-    tau$country <- country
+    tau$country <- countrycode(country, "iso3c", "country.name")
     
     tau
     
   })
   
   maxes <- res %>% 
-    pivot_longer(-country) %>% 
+    pivot_longer(-country) %>%
     group_by(country) %>% 
-    filter(value == max(value)) %>%
+    slice_max(value, n = 1, with_ties = F) %>%
     rename("Max Type" = name, "Max" = value)
   
   res <- inner_join(res, maxes, by = "country") %>%
-    select(country, everything())
+    select(country, everything()) %>%
+    mutate(across(c(-country, -`Max Type`), ~ifelse(
+      is.nan(.x) | .x == -Inf, NA, .x
+    ))) %>%
+    arrange(desc(Max))
   
   return(res)
 }
