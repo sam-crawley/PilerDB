e71791be042f4da119a5ce0715e61f18f7bff231
Sam Crawley <crawlesa@staff.vuw.ac.nz>
2021-06-30 15:41:37 +1200

    Added loading of afrobarometer data

--- read_data/afro.R
+++ read_data/afro.R
@@ -5,5 +5,27 @@ library(haven)
 # Q2A - Language
 # Q84 - Ethnicity
 
-
-data <- read_sav("Divided/data/afrobarom/r7_merged_data_34ctry.release.sav")
+read.data.afro <- function() {
+  
+  data <- read_sav("Divided/data/afrobarom/r7_merged_data_34ctry.release.sav")
+  
+  data <- data %>%
+    mutate(across(c(Q99, Q98, Q2A, Q84, COUNTRY), haven::as_factor)) %>%
+    mutate(across(c(Q99, Q98, Q2A, Q84), fct_explicit_na)) %>%
+    filter(COUNTRY != "eSwatini") %>%
+    rename(
+      "Party" = Q99,
+      "Language" = Q2A,
+      "Religion" = Q98,
+      "Ethnicity" = Q84,
+      "Country" = COUNTRY
+    ) %>%
+    mutate(Year = 2018) %>%
+    mutate(Party = fct_collapse(Party,
+       "None/Missing/DK" = c("Missing", "Would not vote", "Refused", "Don't know", 
+       "Not asked in the country")
+    )) %>%
+    mutate(across(c(Religion, Ethnicity, Language), ~fct_relabel(.x, ~str_replace(.x, "Missing", "\\(Missing\\)"))))
+  
+  return(data)
+}
