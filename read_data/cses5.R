1f85e5a2f4138882c102496791aea98ff13f1282
Sam Crawley <crawlesa@staff.vuw.ac.nz>
2021-11-27 16:32:42 +1300

    Handle country-specific recodes required for CSES5

--- read_data/cses5.R
+++ read_data/cses5.R
@@ -22,10 +22,8 @@ data.spec <- list(
     # Strip out country prefixes from levels
     levels(data$Party) <- str_remove(levels(data$Party), "^\\d+\\. \\w+ - \\s*")
     
-    # Special case for Taiwan, where Taiwanese is coded as a "generic" category
-    data <- data %>% 
-      mutate(Language = fct_expand(Language, "Taiwanese")) %>% 
-      mutate(Language = replace(Language, Country == "Taiwan" & Language == "980. [SEE ELECTION STUDY NOTES]", "Taiwanese"))
+    # Deal with the country-specific recodes for Language/Religion
+    data <- do.country.recodes(data)
     
     for (var in c("Language", "Religion")) {
       levels(data[[var]]) <- str_remove(levels(data[[var]]), "^\\d+\\. ")
@@ -50,3 +48,149 @@ cat.defs <- list(
                 "[SEE ELECTION STUDY NOTES]", "OTHER: NOT SPECIFIED")
   )
 )
+
+country.recodes <- list(
+  Language = list(
+    "Brazil|2018" = list(
+      "Esperanto" = "980. [SEE ELECTION STUDY NOTES]",
+      "Indigenous language / \"caipanga\" / \"tupi guarani\"" = "981. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Iceland|2016" = list(
+      "Icelandic and English" = "980. [SEE ELECTION STUDY NOTES]",
+      "Faroese" = "981. [SEE ELECTION STUDY NOTES]",
+      "Sign Language" = "982. [SEE ELECTION STUDY NOTES]",
+      "Icelandic and Bulgarian" = "983. [SEE ELECTION STUDY NOTES]",
+      "Icelandic and Danish" = "984. [SEE ELECTION STUDY NOTES]",
+      "Icelandic and Norwegian" = "985. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Iceland|2017" = list(
+      "Nepalese" = "980. [SEE ELECTION STUDY NOTES]",
+      "Faroese" = "981. [SEE ELECTION STUDY NOTES]"
+    ),
+    "New Zealand|2017" = list(
+      "Amharic" = "980. [SEE ELECTION STUDY NOTES]",
+      "Khmer" = "981. [SEE ELECTION STUDY NOTES]",
+      "Fijian" = "982. [SEE ELECTION STUDY NOTES]",
+      "Nepali" = "983. [SEE ELECTION STUDY NOTES]",
+      "Samoan" = "984. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Switzerland|2019" = list(
+      "Romansh (Switzerland)" = "980. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Taiwan|2016" = list(
+      "Taiwanese" = "980. [SEE ELECTION STUDY NOTES]",
+      "Both Mandarin and Taiwanese" = "981. [SEE ELECTION STUDY NOTES]",
+      "Both Mandarin and Hakka" = "982. [SEE ELECTION STUDY NOTES]",
+      "Both Mandarin and other Chinese dialect" = "983. [SEE ELECTION STUDY NOTES]",
+      "Both Taiwanese and Hakka" = "984. [SEE ELECTION STUDY NOTES]",
+      "Aboriginal language" = "985. [SEE ELECTION STUDY NOTES]",
+      "Other Chinese dialect" = "986. [SEE ELECTION STUDY NOTES]",
+      "Both Mandarin and Aboriginal language" = "987. [SEE ELECTION STUDY NOTES]",
+      "Mandarin, Taiwanese and Hakka" = "988. [SEE ELECTION STUDY NOTES]",
+      "Burmese" = "989. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Taiwan|2020" = list(
+      "Taiwanese" = "980. [SEE ELECTION STUDY NOTES]",
+      "Aboriginal language" = "981. [SEE ELECTION STUDY NOTES]",      
+      "Other Chinese dialect" = "982. [SEE ELECTION STUDY NOTES]",
+      "Both Mandarin and Taiwanese" = "983. [SEE ELECTION STUDY NOTES]",
+      "Both Mandarin and Hakka" = "984. [SEE ELECTION STUDY NOTES]",
+      "Both Taiwanese and Hakka" = "985. [SEE ELECTION STUDY NOTES]",
+      "Both Mandarin and other Chinese dialect" = "986. [SEE ELECTION STUDY NOTES]",
+      "Mandarin, Taiwanese and English" = "987. [SEE ELECTION STUDY NOTES]",
+      "Both Mandarin and English" = "988. [SEE ELECTION STUDY NOTES]",
+      "Mandarin, Taiwanese and Hakka" = "989. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Turkey|2018" = list(
+      "Uzbek" = "980. [SEE ELECTION STUDY NOTES]",
+      "Turkmen" = "981. [SEE ELECTION STUDY NOTES]"
+    )
+  ),
+  Religion = list(
+    "Australia|2019" = list(
+      "Uniting Church in Australia" = "1420. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Brazil|2018" = list(
+      "Mormon Church, Jehovah Witness" = "1599. [SEE ELECTION STUDY NOTES]",
+      "Candombla (African religion) Umbanda" = "7100. [SEE ELECTION STUDY NOTES]",
+      "Espiritualism" = "7200. [SEE ELECTION STUDY NOTES]",
+      "Seisho-No-Ie, World Messianic Church, Perfect Liberty" = "7900. [SEE ELECTION STUDY NOTES]",
+      "Santo Daime, Esoteric" = "7901. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Germany|2017" = list(
+      "Evangelical Church in Germany (excluding Free Churches)" = "1298. [SEE ELECTION STUDY NOTES]",
+      "Protestant Free Church" = "1299. [SEE ELECTION STUDY NOTES]"
+    ),
+    "United Kingdom|2017" = list(
+      "Other - Protestant" = "1298. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Ireland|2016" = list(
+      "Quaker" = "1298. [SEE ELECTION STUDY NOTES]",
+      "Humanist" = "9001. [SEE ELECTION STUDY NOTES]",
+      "Jedi Knight" = "9002. [SEE ELECTION STUDY NOTES]",
+      "Pastafarianism" = "9003. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Italy|2018" = list(
+      "Asian Religion" = "9001. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Montenegro|2016" = list(
+      "Montenegrin Orthodox" = "1698. [SEE ELECTION STUDY NOTES]",
+      "Serbian Orthodox" = "1699. [SEE ELECTION STUDY NOTES]"
+    ),
+    "South Korea|2016" = list(
+      "Catholic (not further specified)" =  "1199. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Sweden|2018" = list(
+      "Catholic, not further specified" = "1199. [SEE ELECTION STUDY NOTES]",
+      "Orthodox, not further specified" = "1699. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Taiwan|2016" = list(
+      "Buddhist and Taoist" = "9001. [SEE ELECTION STUDY NOTES]",
+      "Confucianism, Buddhism and Taoism" = "9002. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Taiwan|2020" = list(
+      "Buddhist and Taoist" = "9001. [SEE ELECTION STUDY NOTES]",
+      "Falun Gong" = "9002. [SEE ELECTION STUDY NOTES]",
+      "Buddhism and Christianity" = "9003. [SEE ELECTION STUDY NOTES]",
+      "Nine Lotus Sacred Path" = "9004. [SEE ELECTION STUDY NOTES]"
+    ),
+    "Turkey|2018" = list(
+      "Zoroastrian" = "9001. [SEE ELECTION STUDY NOTES]"
+    ),
+    "United States|2016" = list(
+      "Christian, not further specified, inter- or non-denominational" = "1001. [SEE ELECTION STUDY NOTES]",
+      "Evangelical Churches (Evangelical Covenant Church, Evangelical Free Church, EFC, or EFCA)" = "1298. [SEE ELECTION STUDY NOTES]",
+      "Quakers; Friends" = "1299. [SEE ELECTION STUDY NOTES]",
+      "Syrian Orthodox, Armenian Orthodox" = "1699. [SEE ELECTION STUDY NOTES]",
+      "American Indian religions; Native American religions" = "7900. [SEE ELECTION STUDY NOTES]",
+      "Catholic and Protestant" = "9001. [SEE ELECTION STUDY NOTES]",
+      "Messianic Judaism; Jews for Jesus" = "9002. [SEE ELECTION STUDY NOTES]",
+      "More than one major religion (e.g., Christian, Jewish, Muslim, etc.)" = "9003. [SEE ELECTION STUDY NOTES]",
+      "Religious Science; Science of Mind (not Scientology; not Christian Scientists); Centers for Spiritual Living" = "9004. [SEE ELECTION STUDY NOTES]",
+      "Unity; Unity Church; Christ Church Unity" = "9005. [SEE ELECTION STUDY NOTES]"
+    )
+    
+  )
+)
+
+do.country.recodes <- function(data) {
+  for(var in names(country.recodes)) {
+    for(id in names(country.recodes[[var]])) {
+      res <- str_split(id, "\\|", simplify = T)
+      country <- res[[1]]
+      year <- res[[2]]
+      
+      recodes <- country.recodes[[var]][[id]]
+      
+      for (new.cat in names(recodes)) {
+        old.cat <- recodes[[new.cat]]
+        
+        data <- data %>% 
+          mutate(across(all_of(var), ~fct_expand(.x, new.cat))) %>% 
+          mutate(across(all_of(var), ~replace(.x, Country == country & Year == year & .x == old.cat, new.cat)))
+      }
+    }
+  }
+  
+  data
+}
