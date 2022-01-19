3f4241a2659eef223ccc9d62f09d9fabb0ac7baa
Sam Crawley <crawlesa@staff.vuw.ac.nz>
2022-01-19 13:52:04 +1300

    Added remaining latin barometer data

---
+++ read_data/ltb1995.R
@@ -0,0 +1,35 @@
+data.spec <- list(
+  file.name = "Divided/datasets/latino/1995/Latinobarometro_1995_data_english_v2014_06_27.dta",
+  file.type = 'dta',
+  file.encoding = "latin1",  
+  question.text = c(
+    "Party" = "If there were elections tomorrow, which party would you vote for?",
+    "Religion" = "What is your religion?"
+  ),
+  skip.countries = list(),
+  country.format = 'country.name',
+  field.def = c(
+    "Party" = "p33",
+    "Religion" = "p9",
+    "Language" = NA,
+    "Ethnicity" = NA,
+    "Country" = "pais",
+    "Year" = NA,
+    "Weight" = "wt"
+  ),
+  fixups = function(data) {
+    data %>% mutate(Year = 1995)
+  }
+)
+
+cat.defs <- list(
+  Party = list(
+    "Missing" = c("No responde", "No sabe", "Voto secreto", "Vota por personas, no por partidos", "No documentado", "No aplica"),
+    "Other" = c("Otros (partidos nacionales y provinciales)", "Vota nulo/Blanco", "No vota/Ninguno", "No inscrito/No ten\u{c3}\u{ad}a edad")
+  ),
+  Religion = list(
+    "Missing" = c("Not applicable", "No answer/Refused", "DonÂ´t know"),
+    "Other" = c("Others"),
+    "No Religion" = c("Believer, not belonging to any church", "Agnostic", "Atheist", "None")
+  )
+)
---
+++ read_data/ltb2007.R
@@ -0,0 +1,45 @@
+data.spec <- list(
+  file.name = "Divided/datasets/latino/2007/Latinobarometro_2007_datos_eng_v2014_06_27.dta",
+  file.type = 'dta',
+  file.encoding = "latin1",  
+  question.text = c(
+    "Party" = "If elections were held this Sunday, which party would you vote for?",
+    "Religion" = "What is your religion?",
+    "Language" = "What is your native language?",
+    "Ethnicity" = "What ethnicity or race do you identify best with?"
+  ),
+  skip.countries = list(),
+  country.format = 'country.name',
+  field.def = c(
+    "Party" = "p64st",
+    "Religion" = "s4",
+    "Language" = "s22",
+    "Ethnicity" = "s9",
+    "Country" = "idenpa",
+    "Year" = NA,
+    "Weight" = "wt"
+  ),
+  fixups = function(data) {
+    data %>% mutate(Year = 2007)
+  }
+)
+
+cat.defs <- list(
+  Party = list(
+    "Missing" = c("No responde", "No sabe"),
+    "Other" = c("Otros (partidos nacionales y provinciales)", "Vota nulo/Blanco", "No vota/Ninguno", "No inscrito/No ten\u{c3}\u{ad}a edad")
+  ),
+  Language = list(
+    "Missing" = c("Not applicable", "Not asked"),
+    "Other" = c("Other")
+  ),
+  Religion = list(
+    "Missing" = c("Not applicable", "No answer/Refused", "DonÂ´t know"),
+    "Other" = c("Others"),
+    "No Religion" = c("Believer, not belonging to any church", "Agnostic", "Atheist", "None")
+  ),
+  Ethnicity = list(
+    "Missing" = c("Not applicable", "No answer/Refused", "DonÂ´t know"),
+    "Other" = c("Other race")
+  )
+)
