38279a5a02846a72e2a5a427e77ce534fafbd8b4
Sam Crawley <sam@crawley.nz>
2022-01-31 17:10:02 +1300

    Another rework of the report

--- check_high_group_missing.R
+++ check_high_group_missing.R
@@ -21,30 +21,49 @@ get.high.group.missing <- function(tabs) {
       filter(! Group %in% cats.to.drop) %>%
       group_by(Group) %>% 
       mutate(missing.percent = n / sum(n), total.percent = sum(n) / sample.size) %>% 
-      filter(Party == "Missing")
+      filter(Party == "Missing") %>%
+      ungroup()
     
-    mp <- high.missing %>% arrange(desc(total.percent)) %>% pull(missing.percent) %>% set_names(high.missing$Group)
-    
-    if (length(mp) <= 1)
+    if (nrow(high.missing) <= 1)
       return (NULL)
     
+    mp <- high.missing %>% arrange(desc(total.percent))
+    top.group <- mp$Group[[1]]
+    
     high.missing <- high.missing %>%
-             mutate(max.percent = if_else(names(mp)[[1]] == Group, mp[[2]], mp[[1]])) %>%
-             filter(missing.percent - max.percent > -0.2 & total.percent >= 0.10 & missing.percent >= 0.5)
+            mutate(comp.group = if_else(Group == top.group, mp$Group[[2]], top.group)) %>%
+            mutate(max.percent = if_else(Group == top.group, mp$missing.percent[[2]], mp$missing.percent[[1]])) %>%
+            mutate(missing.diff = missing.percent - max.percent) %>%
+            mutate(flag = if_else(missing.diff >= 0.20, T, F)) %>%
+            filter(total.percent >= 0.10 & missing.percent >= 0.5)
     
     if (nrow(high.missing) > 0) {
-      return (tibble(
-        datasource = country,
-        group = group,
-        group.name = high.missing$Group,
-        group.size = high.missing$total.percent,
-        missing = high.missing$missing.percent
-      ))
+      return(
+        high.missing %>% 
+          mutate(datasource = country, group = group) %>%
+          rename(
+            missing = missing.percent,
+            group.name = Group,
+            group.size = total.percent,
+            comp.missing = max.percent
+          ) %>%
+          select(
+            datasource,
+            group,
+            group.name,
+            group.size,
+            missing,
+            comp.group,
+            comp.missing,
+            missing.diff,
+            flag
+          )
+      )
     }
     
     return (NULL)
   })
   
-  res %>% mutate(across(c(group.size, missing), ~round(.x, 3))) %>%
+  res %>% mutate(across(c(group.size, missing, comp.missing, missing.diff), ~round(.x, 3))) %>%
     arrange(desc(missing))
 }
