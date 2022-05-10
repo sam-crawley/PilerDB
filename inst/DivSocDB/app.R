72df058cf4c6fd3dd7dd4f63542add32d456379a
Sam Crawley <sam@crawley.nz>
2022-05-11 09:51:55 +1200

    Fixes to shiny server logging config

--- inst/DivSocDB/app.R
+++ inst/DivSocDB/app.R
@@ -1,7 +1,7 @@
 library(DivSocDB)
 
 logger <- log4r::create.logger()
-log4r::logfile(logger) <- '/var/log/shiny/piler-dashboard.log'
+log4r::logfile(logger) <- '/var/log/shiny-server/piler-dashboard.log'
 log4r::level(logger) <- 'DEBUG'
 
-DivSocDB::divSocApp()
+DivSocDB::divSocApp(logger = logger)
