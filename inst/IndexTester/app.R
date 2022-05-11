4616b7592d5aaa60f6fa8139a87ab90a27b3ba6f
Sam Crawley <crawlesa@staff.vuw.ac.nz>
2022-05-11 14:59:52 +1200

    Attach package before launching shiny app
    Because we call all our internal functions (and access the piler data
    object) as if it were attached.

--- inst/IndexTester/app.R
+++ inst/IndexTester/app.R
@@ -1,1 +1,3 @@
+library(PilerDB)
+
 PilerDB::launchIndexTester()
--- inst/PilerDash/app.R
+++ inst/PilerDash/app.R
@@ -1,3 +1,5 @@
+library(PilerDB)
+
 logger <- log4r::create.logger()
 log4r::logfile(logger) <- '/var/log/shiny-server/piler-dashboard.log'
 log4r::level(logger) <- 'DEBUG'
