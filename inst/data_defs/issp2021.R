439194b7f8f067f0b2f4163aaa0c04f7b4bf860a
Sam Crawley <sam@crawley.nz>
2025-04-08 14:54:39 +1200

    Tweaks to ISSP2021

--- inst/data_defs/issp2021.R
+++ inst/data_defs/issp2021.R
@@ -26,27 +26,40 @@ data.spec <- list(
     data <- coalese.vars(data, str_subset(names(data), "RELIG$"), "relig")
     data <- coalese.vars(data, str_subset(names(data), "ETHN1$"), "ethn")
     
+    # Handle missing (etc.) based on response code
+    data <- data %>%
+      mutate(across(c(prty, relig, ethn), ~if_else(str_detect(.x, "^\\-\\d"), "Missing", .x))) %>%
+      mutate(relig = if_else(str_detect(relig, "^0\\."), "No Religion", relig)) %>%
+      mutate(relig = str_replace(relig, "Yes, consider to belong: ", "")) %>%
+      mutate(across(c(prty, relig, ethn), ~str_replace(.x, "^\\d+\\.\\s+", "")))
+    
     data
   }
 )
 
 cat.defs <- list(
   Party = list(
-    "Missing" = c("-4. NAP (Code 2, -4, -7 in VOTE_LE)", "-9. No answer", "-8. Don't know, don't remember", "-7. Refused",
-                  "-4. NAP (Code 2, -4, in VOTE_LE)", "94. Mixed vote (Candidates of several lists)",
-                  "-8. Don't know, don't remember", "-1. Not available", "-8. Can't say", "-8. Don't remember",
-                  "-8. Don't know/ Can't remember", "-9. No answer + Don't remember", "96. Invalid ballot"),
-    "Other" = c("95. Other party", "90. Independent candidate", "18. Other", "4. Independent", "4. Other candidate",
-                "7. Other")
+    "Missing" = c("Invalid ballot", "Invalid ballot (No second vote)"),
+    "Other" = c("Other party", "Independent candidate", "Other", "Independent", "Other candidate", "Other Party",
+                "Mixed vote (Candidates of several lists)")
   ),
   Religion = list(
-    "Missing" = c("-9. No answer", "-7. Refused", "-9. No answer, Don't know", "-8. Can't choose"),
-    "Other" = c("11. Yes, consider to belong to a religion but: No answer as to which religion",
-                "5. Other", "7. Other non-christians", "13. Other religions", "12. Other Asian religions",
-                "970. Other non-Christian religions", "9. Other Asian religions", "10. Other religions",
-                "5. Other Non-Christians", "960. Other religions", "10. Other religious denomination",
-                "10. Yes, consider to belong: Other religion", "12. Other religions"),
-    "No Religion" = c("0. No religion", "0. No, do not belong to any religion or denomination",
-                      "0. No religion/ None", "0. No, do not consider to belong to a religion")
+    "Missing" = c(),
+    "Other" = c("Yes, consider to belong to a religion but: No answer as to which religion",
+                "Other", "Other non-christians", "Other religions", "Other Asian religions",
+                "Other non-Christian religions", "Other Non-Christians", "Other religious denomination",
+                "Other religion", "Other non-Christian Denomination",
+                "Other Non-Christian", "Inter- or Non-denominational", "Other religion", "Other Religions",
+                "Other Asian Religions", "Other (please specify)", "Other (specify)"),
+    "No Religion" = c("Atheist")
+  ),
+  Ethnicity = list(
+    "Missing" = c("No ethnic affiliation", "None", "No, not belonging to an indigenous group",
+                  "I do not consider as belonging to a particular group",
+                  "Other North African and Middle Eastern"),
+    "Other" = c("Both parents born in Austria", "Both parents born abroad", "One parent born in abroad",
+                "Danish group", "Religious group", "Migrants group", "Returnees", "Others", "Other North American",
+                "Other countries", "Other (please specify)", "Other non western",
+                "Other western")
   )
 )
